596474161345f535566bef09667eccbeb4b979d8
Kijimj7 <pinkokinawa84@gmail.com>
2021-11-22 15:14:37 -0800

    Initial Commit

---
+++ FindawordProject/Assets/Oculus/Voice/Demo/Scripts/BuiltinDemo/WitActivation.cs
@@ -0,0 +1,45 @@
+﻿/**************************************************************************************************
+ * Copyright : Copyright (c) Facebook Technologies, LLC and its affiliates. All rights reserved.
+ *
+ * Your use of this SDK or tool is subject to the Oculus SDK License Agreement, available at
+ * https://developer.oculus.com/licenses/oculussdk/
+ *
+ * Unless required by applicable law or agreed to in writing, the Utilities SDK distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ **************************************************************************************************/
+
+using Oculus.Voice;
+using UnityEngine;
+
+public class WitActivation : MonoBehaviour
+{
+    private AppVoiceExperience _voiceExperience;
+    private void OnValidate()
+    {
+        if (!_voiceExperience) _voiceExperience = GetComponent<AppVoiceExperience>();
+    }
+
+    private void Start()
+    {
+        _voiceExperience = GetComponent<AppVoiceExperience>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("*** Pressed Space bar ***");
+            ActivateWit();
+        }
+    }
+
+    /// <summary>
+    /// Activates Wit i.e. start listening to the user.
+    /// </summary>
+    public void ActivateWit()
+    {
+        _voiceExperience.Activate();
+    }
+}
